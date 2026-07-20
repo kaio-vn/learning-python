e3a2d8a88d7765c31dcdbbad55b4fb373f8074a0
Kaio <kaio.viniciuscruz0@gmail.com>
2026-07-20 17:04:55 -0300

    Python1st

---
+++ main.py
@@ -0,0 +1,7 @@
+ajective1 = input("Enter an adjective: ")
+noun1 = input("Enter a noun: ")
+ajective2 = input("Enter an adjective: ")
+verb1 = input("Enter a verb: ")
+
+
+print(f"Today I went to a {ajective1} zoo. ")
